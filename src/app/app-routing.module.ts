925892dbded3914c4cf8ec6e9eec4ebb0935347e
Muhammad Awais <muhammad.awais@royalcyber.com>
2021-08-25 00:11:06 +0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '/',
+    path: '',
     loadChildren: () => import('./components/homepage/homepage.module').then(m => m.HomepageModule)
   },
   {
