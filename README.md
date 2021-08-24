c931e3abc4f12f5d061497612dd502ecf4415697
Muhammad Awais <muhammad.awais@royalcyber.com>
2021-08-25 00:10:04 +0500

    app init with lazyLoading modules

---
+++ README.md
@@ -0,0 +1,8 @@
+# Reactive State Management using Ngrx
+
+## Ngrx
+https://ngrx.io/
+
+## Refrence
+https://www.youtube.com/watch?v=f97ICOaekNU
+
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '/',
+    loadChildren: () => import('./components/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'homepage',
+    loadChildren: () => import('./components/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./components/products/products.module').then(m => m.ProductsModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
